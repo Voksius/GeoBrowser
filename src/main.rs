bd85abf0ae08631656212ef4646c94f9e92caa89
gmdEzreal <gmdezreal@gmail.com>
2021-04-27 18:31:32 -0700

    added menu system

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,35 @@ mod level;
 fn main() {
 
     let client: Client = Client::new();
-    let mut lvl_id: String = String::new();
 
-    println!("Enter the level ID..");
+    loop
+    {
 
-    stdin()
-        .read_line(&mut lvl_id)
-        .expect("Line read error");
+        println!("0. Exit");
+        println!("1. Search Level by ID");
+        println!("What do you want to do?");
 
-    level::lookup(lvl_id, &client);
-    
+        let mut choice: String = String::new();
+
+        stdin()
+            .read_line(&mut choice)
+            .expect("Line read error.");
+
+        if choice.trim() == "0" { break; }
+
+        if choice.trim() == "1"
+        {
+
+            let mut lvl_id: String = String::new();
+
+            println!("Enter the level ID..");
+
+            stdin()
+                .read_line(&mut lvl_id)
+                .expect("Line read error");
+
+            level::lookup(lvl_id, &client);
+            
+        }
+    }
 }
